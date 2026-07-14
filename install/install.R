d3aa3703b753661ef00431c07449e6701bbdfaa0
harrymatthews50 <harrymatthews50@gmail.com>
2026-07-14 16:10:40 +0200

    cause install.R to exit when a package is not installed

--- install/install.R
+++ install/install.R
@@ -1,9 +1,13 @@
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-BiocManager::install("limma")
-BiocManager::install("edgeR") # prerequisite for sva
-BiocManager::install("sva")
-install.packages("readr")
-install.packages("dplyr")
-install.packages("jsonlite")
-install.packages("Rfit")
-install.packages("emmeans")
+install_and_check <- function(pkg, bioc = FALSE) {
+    if (bioc) BiocManager::install(pkg) else install.packages(pkg)
+    if (!requireNamespace(pkg, quietly = TRUE))
+        stop("Failed to install: ", pkg)
+}
+install_and_check("limma", TRUE)
+install_and_check("edgeR", TRUE) # prerequisite for sva
+install_and_check("sva", TRUE)
+install_and_check("readr")
+install_and_check("dplyr")
+install_and_check("jsonlite")
+install_and_check("Rfit")
+install_and_check("emmeans")
