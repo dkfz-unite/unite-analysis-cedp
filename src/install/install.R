dbdf9106ea819580038c8905dbefd8803b916145
harrymatthews50 <harrymatthews50@gmail.com>
2026-07-14 16:39:23 +0200

    add material to test docker build

---
+++ src/install/install.R
@@ -0,0 +1,13 @@
+install_and_check <- function(pkg, bioc = FALSE) {
+    if (bioc) BiocManager::install(pkg) else install.packages(pkg)
+    if (!requireNamespace(pkg, quietly = TRUE))
+        stop("Failed to install: ", pkg)
+}
+install_and_check("limma", TRUE)
+install_and_check("edgeR", TRUE) # prerequisite for sva
+install_and_check("sva", TRUE)
+install_and_check("readr")
+install_and_check("dplyr")
+install_and_check("jsonlite")
+install_and_check("Rfit")
+install_and_check("emmeans")
